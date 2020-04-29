b93d97194f493453fcf20fb475fa25ac217db999
taesu-park <tsoo1014@gmail.com>
2020-04-29 12:11:24 +0900

    200429 | Django add User

--- todo_django/todos/models.py
+++ todo_django/todos/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
-class Furits(models.Model):
-    name = models.CharField(max_length=50)
-    descript = models.TextField()
-    price = models.FloatField()
-    quantity = models.IntegerField()
-    cdate = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return [self.id ,self.name, self.descript]
+class User(AbstractUser):
+    pass
+
+class Todo(models.Model):
+    title = models.TextField()
+    is_completed = models.BooleanField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
