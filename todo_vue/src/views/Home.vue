27b75a3d10bd19ea65b99ab7c63adc6f1c2e887c
taesu-park <tsoo1014@gmail.com>
2020-05-01 17:52:05 +0900

    200501 | Add TodoListComponent

---
+++ todo_vue/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <TodoList/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TodoList from '@/components/TodoList.vue'
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  components: {
+    TodoList
+  },
+  data() {
+    return{
+      todos : [],
+    }
+  },
+  mounted() {
+    // axios요청
+    axios.get('http://127.0.0.1:8000/api/v1/todos/')
+    .then(response =>{
+      console.log(response) // 만약 오류가 발생하게되면 ESLinst 설정을 package.json에 추가
+    }
+
+    )
+  },
+}
+</script>
