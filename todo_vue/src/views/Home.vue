cd86f7425288b2f6bbad2d9933d7c26e8bbff900
taesu-park <tsoo1014@gmail.com>
2020-05-01 20:09:33 +0900

    200501 | add TodoForm

--- todo_vue/src/views/Home.vue
+++ todo_vue/src/views/Home.vue
@@ -1,33 +1,67 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <TodoList/>
+    <!-- PascalCase, uppercamelcase-->
+    <!--이벤트 리스터 등록-->
+    <TodoForm @todoCreate-event="TodoCreate"/>
+    <TodoList :todos="todos"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import TodoForm from '@/components/TodoForm.vue'
 import TodoList from '@/components/TodoList.vue'
 import axios from 'axios'
 
 export default {
-  name: 'Home',
+  name: 'home',
   components: {
-    TodoList
+    TodoList,
+    TodoForm
   },
-  data() {
+  data(){
     return{
       todos : [],
     }
   },
-  mounted() {
+  methods:{
+    TodoCreate(title){
+      console.log("==부모 컴포넌트==")
+      console.log(title)
+      const data = {
+        title:title,
+        user: 1
+      }
+      // request.POST인 경우는 반드시 FormData!
+      // const formData = new FormData()
+      // formData.append('title', title)
+      // formData.append('user',1)
+      axios.post('http://127.0.0.1:8000/api/v1/todos/', data)
+      .then(response =>{
+        console.log(response)
+        this.todos.push(response.data)
+      })
+      .catch(error =>{
+        console.log(error)
+      })
+    },
+    getTodos(){
     // axios요청
     axios.get('http://127.0.0.1:8000/api/v1/todos/')
     .then(response =>{
-      console.log(response) // 만약 오류가 발생하게되면 ESLinst 설정을 package.json에 추가
+      console.log(response.data[0].title) // 만약 오류가 발생하게되면 ESLinst 설정을 package.json에 추가
+      this.todos = response.data
+    })
+    .catch(error =>{
+      console.log(error)
+    })
+  }
+},
+    mounted(){
+      this.getTodos()
     }
-
-    )
-  },
 }
+
 </script>
+
