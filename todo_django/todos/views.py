31df15e53e6facf87f64bfdc55a4666d9484c18d
taesu-park <tsoo1014@gmail.com>
2020-05-01 18:28:56 +0900

    200501 | add TodoCreate

--- todo_django/todos/views.py
+++ todo_django/todos/views.py
@@ -4,8 +4,17 @@ from rest_framework.decorators import api_view
 from .models import Todo
 from .serializers import TodoSerializers
 # Create your views here.
-@api_view(['GET'])
+
+# GET /todos/ : 전체 todos목록 가져오기
+# POST /todos/ : todos 등록(저장하기)
+@api_view(['GET','POST'])
 def todo_index_create(request):
-    todos = Todo.objects.all()
-    serializers = TodoSerializers(todos, many=True)
-    return Response(serializers.data)
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializers = TodoSerializers(todos, many=True)
+        return Response(serializers.data)
+    else:
+        serializers = TodoSerializers(data=request.POST)
+        if serializers.is_valid(raise_exception=True):
+            serializers.save()
+            return Response(serializers.data)
